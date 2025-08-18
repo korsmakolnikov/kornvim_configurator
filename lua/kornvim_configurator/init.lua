4ef192d1772597b1eba8cd7cf2b47513a41b6e94
korsmakolnikov <korsmakolnikov@gmail.com>
2025-08-19 00:13:20 +0200

    Raw setup implementation

--- lua/kornvim_configurator/init.lua
+++ lua/kornvim_configurator/init.lua
@@ -1,19 +1,89 @@
 -- lua/kornvim_configurator/init.lua
-
--- Define an empty table that will represent your plugin.
+-- personal vim setting collection
+local options = vim.opt
+local options_local = vim.opt_local
+local globals = vim.g
 local M = {}
 
--- Load the core module, where the main logic will be.
-require("kornvim_configurator.core")
-
---- The `setup()` function is a common convention for Neovim plugins.
--- It allows the user to configure your plugin by passing an options table.
--- @param opts table of configuration options
-function M.setup(opts)
-  -- Add your configuration logic here.
-  -- e.g., if opts.some_option then ... end
-  print("Kornvim Configurator is set up!")
+local function setBackupFolder()
+  options.backupdir = '~/.config/nvim/backup'
 end
 
--- Return the M table, making its functions accessible from the outside.
+local function setBaseCompletitionOptions()
+  options.wildmode = 'longest,list'
+  options.completeopt = 'menuone,noinsert,noselect'
+end
+
+local function setTheme()
+  options.guifont = 'Fira Code Font:h14,Symbols Nerd Font'
+  options.cmdheight = 3
+  options.number = true
+end
+
+local function setEditorBehaviour()
+  options.syntax = 'on'
+  options.mouse = 'a'
+  options.matchpairs = options.matchpairs .. ",<:>"
+  options.splitright = true
+  options.hlsearch = true
+  options.incsearch = true
+
+  -- shorten the messages in the message bar
+  options.shortmess = options.shortmess .. 'c'
+end
+
+local function setGlobals()
+  globals.mapleader = ","
+  globals.loaded_python_provider = '0'
+  globals.goyo_width = '80'
+  _G.Original_folder = vim.loop.cwd()
+end
+
+local function setSpell()
+  -- create the folder site/spell in the vim data folder
+  vim.fn.mkdir(vim.fn.stdpath("data") .. "site/spell", "p")
+  options.spelllang = { "en_us", "it" }
+  options.sessionoptions = 'curdir,folds,globals,help,tabpages,terminal,winsize'
+  options.spell = true
+end
+
+function M.setTab(tab)
+  options.tabstop = tab
+  options.shiftwidth = tab
+  options.softtabstop = tab
+  options.expandtab = true
+end
+
+function M.editorBaseConfiguration(columns, wrap_flag)
+  wrap_flag = wrap_flag | false
+  options_local.wrap = wrap_flag
+  options.cc = columns
+  options.virtualedit = 'all'
+  options.autoindent = true
+  options.encoding = "utf-8"
+end
+
+local function setClipboard()
+  options.clipboard = options.clipboard .. 'unnamed,unnamedplus'
+end
+
+local function termOptions()
+  options.termguicolors = true
+  options.ttyfast = true
+  options.termguicolors = true
+end
+
+function M.setup()
+  setGlobals()
+  setBackupFolder()
+  M.editorBaseConfiguration('80')
+  termOptions()
+  M.setTab(2)
+  setClipboard()
+  setSpell()
+  setBaseCompletitionOptions()
+  setEditorBehaviour()
+  setTheme()
+end
+
 return M
